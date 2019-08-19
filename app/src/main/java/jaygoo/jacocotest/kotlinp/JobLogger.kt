c5d9a9807622f642d73371c0c434507a8c1d6766
liqiang32 <liqiang32@58ganji.com>
2019-08-19 16:39:01 +0800

    add kotlin test coverage

--- app/src/main/java/jaygoo/jacocotest/kotlinp/JobLogger.kt
+++ app/src/main/java/jaygoo/jacocotest/kotlinp/JobLogger.kt
@@ -5,6 +5,13 @@ object JobLogger {
 
     fun d(msg: String) {
         println("hello the jacoco kotlin test")
+
+        if (msg.length > 2) {
+            println("hello the jacoco  branch1")
+        } else {
+            println("hello the jacoco  branch2")
+        }
+
     }
 
     fun e(e: Exception) {
